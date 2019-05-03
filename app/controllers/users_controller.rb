7e163fc887e045787aef3470805a2f7ef5b3fc02
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-03 17:06:33 +0200

    Add flash notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,33 @@
 
 class UsersController < ApplicationController
   include UsersHelper
-  
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
 
     if @user.save
-        redirect_to new_user_path
+      flash[:success] = 'User successfully created'
+      redirect_to new_user_path
     else
-        render :new
+      flash.now[:error] = @user.errors.full_messages
+      render :new
     end
   end
+
   def edit
     @user = User.find params[:id]
   end
+
   def update
     @user = User.find params[:id]
-    @user = User.update(user_params)
-    redirect_to '/'
+    if @user.update user_params
+      redirect_to edit_user_path, notice: 'User updated'
+    else
+      redirect_to edit_user_path, notice: @user.errors.full_messages
+    end
   end
-  
 end
